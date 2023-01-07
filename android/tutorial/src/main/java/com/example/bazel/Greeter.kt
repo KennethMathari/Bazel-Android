5d675d05047052bccb1d8d97f3f48e64c932dc7f
Kenneth Mathari <mathari580@gmail.com>
2023-01-07 20:47:26 +0300

    migrate to kotlin

---
+++ android/tutorial/src/main/java/com/example/bazel/Greeter.kt
@@ -0,0 +1,10 @@
+package com.example.bazel
+
+/**
+ * A tiny Greeter library for the Bazel Android "Hello, World" app.
+ */
+class Greeter {
+    fun sayHello(): String {
+        return "Hello Bazel! \uD83D\uDC4B\uD83C\uDF31" // Unicode for 👋🌱
+    }
+}
