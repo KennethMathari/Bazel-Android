447288deacb1ddd6b8a97295e74fdc5dd86e24c9
Kenneth Mathari <mathari580@gmail.com>
2023-01-08 23:03:50 +0300

    added viewmodel & livedata dependencies

---
+++ android/tutorial/src/main/java/com/example/bazel/GreeterViewModel.java
@@ -0,0 +1,21 @@
+package com.example.bazel;
+
+//create view model class with LiveData
+//public class GreeterViewModel extends ViewModel{
+//    Greeter greeter = new Greeter();
+//
+//    public String getGreeting() {
+//        return greeter.sayHello();
+//    }
+//}
+//public class GreeterViewModel extends ViewModel {
+//    private String greeting;
+//
+//    public GreeterViewModel(String greeting) {
+//        this.greeting = greeting;
+//    }
+//
+//    public String getGreeting() {
+//        return greeting;
+//    }
+//}
