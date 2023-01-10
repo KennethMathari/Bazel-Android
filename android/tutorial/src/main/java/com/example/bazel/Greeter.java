3df7aa5effdfa9574d2cf9af49355c6dce84405f
Kenneth Mathari <mathari580@gmail.com>
2023-01-10 18:59:38 +0300

    Successful build-point 1
    -removed some stubborn dependencies i.e material & appcompat
    - updated code in MainActivity. Potential issue with lifecycleowner

--- android/tutorial/src/main/java/com/example/bazel/Greeter.java
+++ android/tutorial/src/main/java/com/example/bazel/Greeter.java
@@ -5,6 +5,6 @@ package com.example.bazel;
  */
 public class Greeter {
   public String sayHello() {
-    return "Test live data! \uD83D\uDC4B\uD83C\uDF31"; // Unicode for 👋🌱
+    return "Hello Bazel! \uD83D\uDC4B\uD83C\uDF31"; // Unicode for 👋🌱
   }
 }
--- android/tutorial/src/main/java/com/example/bazel/MainActivity.java
+++ android/tutorial/src/main/java/com/example/bazel/MainActivity.java
@@ -4,18 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import com.example.bazel.GreeterViewModel;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.lifecycle.ViewModelProvider;
+//import com.example.bazel.GreeterViewModel;
 
 /**
  * Main class for the Bazel Android "Hello, World" app.
  */
-public class MainActivity extends AppCompatActivity {
+//extends AppCompatActivity
+public class MainActivity extends Activity {
 
-    private GreeterViewModel greeterViewModel;
-    Button clickMeButton = findViewById(R.id.clickMeButton);
-    TextView helloBazelTextView = findViewById(R.id.helloBazelTextView);
+//    private GreeterViewModel greeterViewModel;
+
 
 
     @Override
@@ -23,20 +23,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        greeterViewModel = new ViewModelProvider(this).get(GreeterViewModel.class);
-        greeterViewModel.getGreeting();
+        Button clickMeButton = findViewById(R.id.clickMeButton);
+        TextView helloBazelTextView = findViewById(R.id.helloBazelTextView);
 
-        greeterViewModel.greeting.observe(this, greeting -> {
-            helloBazelTextView.setText(greeting);
-        });
+//        greeterViewModel = new ViewModelProvider(this).get(GreeterViewModel.class);
+//        greeterViewModel.getGreeting();
+//
+//        greeterViewModel.greeting.observe(this, greeting -> {
+//            helloBazelTextView.setText(greeting);
+//        });
 
 
-//    Greeter greeter = new Greeter();
+    Greeter greeter = new Greeter();
 
         // Bazel supports Java 8 language features like lambdas!
-//    clickMeButton.setOnClickListener(v ->
-//            helloBazelTextView.setText(greeter.sayHello())
-//    );
+    clickMeButton.setOnClickListener(v ->
+            helloBazelTextView.setText(greeter.sayHello())
+    );
     }
 
 }
